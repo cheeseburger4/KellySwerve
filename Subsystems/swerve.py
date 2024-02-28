1a26389a36a08865ed72d3c18a79f644f62b6b1e
cheeseburger4 <147112825+cheeseburger4@users.noreply.github.com>
2024-02-27 19:10:12 -0800

    i

--- Subsystems/swerve.py
+++ Subsystems/swerve.py
@@ -1,15 +1,20 @@
 from commands2 import Subsystem
+from phoenix6.hardware import TalonFX
+from wpimath.kinematics import ChassisSpeeds, SwerveDrive4Kinematics, SwerveModulePosition, SwerveModuleState
 import phoenix6
 
 class SwerveModule(Subsystem):
     
-    def __init__(self, mod_name, drive_motor_constants, direction_motor_constants, driveID, directionID, coderID):
+    def __init__(self, mod_name, drive_motor_constants, direction_motor_constants):
         self.name = mod_name
         self.drive_const = drive_motor_constants
         self.direction_const = direction_motor_constants
 
-        self.drive_motor = phoenix6.hardware.TalonFX(driveID, "rio")
-        self.direction_motor = phoenix6.hardware.TalonFX(directionID, "rio")
+        self.drive_motor = TalonFX(drive_motor_constants.motor_id, "rio")
+        drive_motor_constants.apply_configuration(self.drive_motor)
+
+        self.direction_motor = TalonFX(direction_motor_constants.motor_id, "rio")
+        direction_motor_constants.apply_configuration(self.direction_motor)
 
     def setDirectionAngle():
         pass
